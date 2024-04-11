f394aa19962051b56efd420b93618c509d0bbf67
Брусенцев Александр Юрьевич <abrusentsev@aviatron.ru>
2024-04-11 15:40:56 +0500

    vector output was added

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
 int main(){
+	std::vector v{1, 2, 3, 4, 5, 6};
+	std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+	std::cout << std::endl;
 	std::cout << "hello world" << std::endl;
 	return 0;	
 }
